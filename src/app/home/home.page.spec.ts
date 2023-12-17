78b3bd8c07db87a0047f4f9ac87b59a2effe4eb5
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2023-12-17 12:09:31 +0100

    Napravljena pocetna stranica sa nekim osnovim funkcionalnostima,ostale stranice samo kreirane

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import {MatIconModule} from '@angular/material/icon';
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
